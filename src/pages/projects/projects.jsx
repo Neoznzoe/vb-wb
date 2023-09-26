34fb9ad98520e40438ceb24fac83a042ab102c83
Victor <vbesson@avmj.fr>
2023-09-26 09:26:34 +0200

    'starting projects page'

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../../components/Header/header";
+import ProjectPage from "../../components/projects-page/projects-page";
 import './projects.scss';
 
 function Projects() {
     return (
         <div>
         <Header/>
-   
+        <div className="projectsPage">
+          <ProjectPage/>
+          </div>
         <div className="noise"></div>
       </div>
       
