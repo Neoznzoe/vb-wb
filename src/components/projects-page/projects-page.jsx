db018ca874c9775e33cb9670dda44cca5d7fceab
neoznzoe <victorbesson59@gmail.com>
2023-09-30 15:05:42 +0200

    picture bz

--- src/components/projects-page/projects-page.jsx
+++ src/components/projects-page/projects-page.jsx
@@ -3,8 +3,10 @@ import React, { useState } from "react";
 
 function ProjectPage() {
   const [projects, setProjects] = useState([
-    { id: 1, name: 'Projet 1', imageUrl: 'path_to_image_1.jpg' },
-    { id: 2, name: 'Projet 2', imageUrl: 'path_to_image_2.jpg' },
+    { id: 1, name: 'Projet 1', imageUrl: '../../assets/img/braden-jarvis-prSogOoFmkw-unsplash.jpg' },
+    { id: 2, name: 'Projet 2', imageUrl: '../../../assets/img/braden-jarvis-prSogOoFmkw-unsplash.jpg' },
+    { id: 3, name: 'Projet 3', imageUrl: '../../../assets/img/braden-jarvis-prSogOoFmkw-unsplash.jpg' },
+    { id: 4, name: 'Projet 4', imageUrl: '../../../assets/img/braden-jarvis-prSogOoFmkw-unsplash.jpg' },
   ]);
 
   const [currentImage, setCurrentImage] = useState("path_to_default_project_image.jpg");
