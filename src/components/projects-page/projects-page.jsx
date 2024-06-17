187f395dc90efc00cbc33c8917561655faa43bae
neoznzoe <victorbesson59@gmail.com>
2024-06-17 18:49:17 +0200

    feat latest update

--- src/components/projects-page/projects-page.jsx
+++ src/components/projects-page/projects-page.jsx
@@ -1,12 +1,20 @@
 import './projects-page.scss';
 import React, { useState } from "react";
+import image1 from '../../assets/img/braden-jarvis-prSogOoFmkw-unsplash.jpg';
+import image2 from '../../assets/img/dan-rogers-lMIvz5b1vRo-unsplash.jpg';
+import image3 from '../../assets/img/maxime-gauthier-aBrM3LLqJX4-unsplash.jpg';
+import image4 from '../../assets/img/michael-d-rnKqWvO80Y4-unsplash.jpg';
 
 function ProjectPage() {
   const [projects, setProjects] = useState([
-    { id: 1, name: 'Projet 1', imageUrl: '../../assets/img/braden-jarvis-prSogOoFmkw-unsplash.jpg' },
-    { id: 2, name: 'Projet 2', imageUrl: '../../../assets/img/braden-jarvis-prSogOoFmkw-unsplash.jpg' },
-    { id: 3, name: 'Projet 3', imageUrl: '../../../assets/img/braden-jarvis-prSogOoFmkw-unsplash.jpg' },
-    { id: 4, name: 'Projet 4', imageUrl: '../../../assets/img/braden-jarvis-prSogOoFmkw-unsplash.jpg' },
+    { id: 1, name: 'Projet 1', imageUrl: image1, option: 'Branding' },
+    { id: 2, name: 'Projet 2', imageUrl: image2, option: 'Web Developpement' },
+    { id: 3, name: 'Projet 3', imageUrl: image3, option: 'Portrait' },
+    { id: 4, name: 'Projet 4', imageUrl: image4, option: 'UX/UI Design' },
+    { id: 5, name: 'Projet 5', imageUrl: image1, option: 'Branding' },
+    { id: 6, name: 'Projet 6', imageUrl: image1, option: 'Game Design' },
+    { id: 6, name: 'Projet 7', imageUrl: image1, option: 'Illlustration' },
+    { id: 6, name: 'Projet 8', imageUrl: image1, option: 'UX/UI Design' },
   ]);
 
   const [currentImage, setCurrentImage] = useState("path_to_default_project_image.jpg");
@@ -34,23 +42,36 @@ function ProjectPage() {
   return (
     <div className="container">
       <div className="photo-section">
-        <img src={currentImage} alt="Current Project" />
+        {currentImage !== "path_to_default_project_image.jpg" ? (
+          <img src={currentImage} alt="" />
+        ) : null}
       </div>
-
+  
       <div className="list-section">
-        <ul>
-          {projects.map(project => (
-            <li 
+      <ul>
+        <li className='li-title'>
+          WORK
+          <span className="project-count">{projects.length}</span>
+        </li>
+        <div className="projects-container">
+        {projects.map(project => (
+          <li 
               key={project.id} 
+              onMouseOver={() => setCurrentImage(project.imageUrl)}
+              onMouseOut={() => setCurrentImage("path_to_default_project_image.jpg")}
               onClick={() => setCurrentImage(project.imageUrl)}
-            >
-              {project.name}
-            </li>
-          ))}
-        </ul>
+          >
+              <span className="arrow">→</span>
+              <span className="project-name">{project.name}</span>
+              <span className="project-option">{project.option}</span>
+          </li>
+        ))}
         </div>
-      </div>
-    );
+      </ul>
+    </div>
+    </div>
+  );
+  
   }
 
 export default ProjectPage;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Header from "../../components/Header/header";
 import Footer from "../../components/footer/footer";
 import './index.scss';
 
 function HomePage() {
+    const textRef = useRef(null);
+
+    useEffect(() => {
+      const text = textRef.current;
+      if (text) {
+          const characters = text.innerText.split("");
+          const angle = 360 / characters.length;
+          text.innerHTML = characters.map(
+              (char, i) => `<span style="transform:rotate(${angle * i}deg)">${char}</span>`
+          ).join("");
+      }
+  }, []);
+
     return (
       <div>
         <Header/>
         <div className="page-wrapper">
           <div className="title">Bonjour,</div>
+          {/* Circle Animated Text Logo HTML */}
+          <div className="circle">
+              <div className="logo"></div>
+              <div className="text">
+                  <p ref={textRef}> Denis Bett - Creative UI/UX  Designer -  </p>
+              </div>
+          </div>
         </div>
         <div className="main-content">
           <Footer/>
         </div>
         <div className="noise"></div>
       </div>
-      
     );
-  }
+}
 
 export default HomePage;
